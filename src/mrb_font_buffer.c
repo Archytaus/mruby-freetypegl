9f299d3f18d14731143c3387bcf8d06670fa6d3e
Ryan Scott <ryan.scott@tickbox.com.au>
2013-07-29 07:34:26 +1000

    Started adding support for font styles.

--- src/mrb_font_buffer.c
+++ src/mrb_font_buffer.c
@@ -16,7 +16,7 @@
 typedef struct {
     float x, y, z;    // position
     float s, t;       // texture
-    float r, g, b, a; // color
+    vec4 color;       // color
 } vertex_t;
 
 static struct RClass* mrb_font_buffer_class = NULL;
@@ -83,11 +83,10 @@ mrb_font_buffer_create(mrb_state *mrb, mrb_value self)
 }
 
 void add_text( vertex_buffer_t * buffer, texture_font_t * font,
-               char* text, vec4 * color )
+               char* text, vec4 color, vec4 color2 )
 {
     vec2 pen = {{0,0}};
     size_t i;
-    float r = color->red, g = color->green, b = color->blue, a = color->alpha;
     for( i=0; i<strlen(text); ++i )
     {
         texture_glyph_t *glyph = texture_font_get_glyph( font, text[i] );
@@ -99,6 +98,7 @@ void add_text( vertex_buffer_t * buffer, texture_font_t * font,
                 kerning = texture_glyph_get_kerning( glyph, text[i-1] );
             }
             pen.x += kerning;
+
             int x0  = (int)( pen.x + glyph->offset_x );
             int y0  = (int)( pen.y - glyph->offset_y );
             int x1  = (int)( x0 + glyph->width );
@@ -107,12 +107,17 @@ void add_text( vertex_buffer_t * buffer, texture_font_t * font,
             float t0 = glyph->t0;
             float s1 = glyph->s1;
             float t1 = glyph->t1;
-            GLuint indices[6] = {0,1,2, 0,2,3};
-            vertex_t vertices[4] = { { x0,y0,0,  s0,t0,  r,g,b,a },
-                                     { x0,y1,0,  s0,t1,  r,g,b,a },
-                                     { x1,y1,0,  s1,t1,  r,g,b,a },
-                                     { x1,y0,0,  s1,t0,  r,g,b,a } };
+            
+            GLuint index = 0;
+            GLuint indices[] = {index, index+1, index+2,
+                                index, index+2, index+3};
+
+            vertex_t vertices[4] = { { x0,y0,0,  s0,t0,  color  },
+                                     { x0,y1,0,  s0,t1,  color2 },
+                                     { x1,y1,0,  s1,t1,  color2  },
+                                     { x1,y0,0,  s1,t0,  color } };
             vertex_buffer_push_back( buffer, vertices, 4, indices, 6 );
+
             pen.x += glyph->advance_x;
         }
     }
@@ -123,14 +128,41 @@ mrb_font_buffer_set_text(mrb_state *mrb, mrb_value self)
 {
   struct mrb_font_buffer* font_buffer = mrb_font_buffer_get_ptr(mrb, self);
 
-  mrb_value text;
-  mrb_get_args(mrb, "S", &text);
+  mrb_value text_value;
+  mrb_sym flag;
+  int arg_count = mrb_get_args(mrb, "S|n", &text_value, &flag);
+  char* text = mrb_string_value_ptr(mrb, text_value); 
 
-  vec4 black = {{0,0,0,1}};
+  if (arg_count == 1 || flag == mrb_intern_cstr(mrb, "normal")) {
+    vec4 black = {{0,0,0,1}};
 
-  vertex_buffer_clear(font_buffer->buffer);
-  add_text(font_buffer->buffer, font_buffer->font, mrb_string_value_ptr(mrb, text), &black);
+    vertex_buffer_clear(font_buffer->buffer);
+    add_text(font_buffer->buffer, font_buffer->font, text, black, black);  
+  } else {
+    if (flag == mrb_intern_cstr(mrb, "cartoon")) {
+      vec4 text_outline  = {{0.0, 0.0, 0.0, 1.0}};
+      vec4 glow =          {{1.0, 1.0, 0.0, 1.0}};
+      vec4 fill_top =      {{1.0, 0.9, 0.0, 1.0}};
+      vec4 fill_bottom =   {{1.0, 0.6, 0.0, 1.0}};
 
+      font_buffer->font->outline_type = 2;
+      font_buffer->font->outline_thickness = 7;
+      add_text(font_buffer->buffer, font_buffer->font, text, text_outline, text_outline);
+
+      font_buffer->font->outline_type = 2;
+      font_buffer->font->outline_thickness = 5;
+      add_text(font_buffer->buffer, font_buffer->font, text, glow, glow);
+
+      font_buffer->font->outline_type = 1;
+      font_buffer->font->outline_thickness = 3;
+      add_text(font_buffer->buffer, font_buffer->font, text, text_outline, text_outline);
+
+      font_buffer->font->outline_type = 0;
+      font_buffer->font->outline_thickness = 0;
+      add_text(font_buffer->buffer, font_buffer->font, text, fill_top, fill_bottom);  
+    }
+  }
+  
   return self;
 }
 
@@ -242,7 +274,7 @@ init_mrb_font_buffer(mrb_state* mrb, struct RClass* mrb_freetype_class)
 
   mrb_define_class_method(mrb, mrb_font_buffer_class, "create", mrb_font_buffer_create, ARGS_REQ(2));
   
-  mrb_define_method(mrb, mrb_font_buffer_class, "text=", mrb_font_buffer_set_text, ARGS_REQ(1));
+  mrb_define_method(mrb, mrb_font_buffer_class, "set_text", mrb_font_buffer_set_text, ARGS_REQ(1));
   mrb_define_method(mrb, mrb_font_buffer_class, "calculate_size", mrb_font_buffer_calculate_size, ARGS_REQ(1));
   
   mrb_define_method(mrb, mrb_font_buffer_class, "bind", mrb_font_buffer_bind, ARGS_REQ(1));
